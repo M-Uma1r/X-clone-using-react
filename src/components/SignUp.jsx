aac77ff7a2e70d2cbaf3a2f42f93490b383097ea
shoaibNadeem <shoaibmemon221@gmail.com>
2024-07-10 16:44:22 +0500

    some changes on main page

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -4,23 +4,27 @@ import './SignUp.css'
 
 export default function SignUp() {
   return (
-    <div className="d-flex align-items-center">
+    <div className="d-flex align-items-center ">
       <div className="logo">
         <SignUplogo />
       </div>
       <div className="details text-white ms-3">
-        <p className='h1'>Happening now</p> <br /> <br />
-        <h1>Join today</h1> <br />
-        <button className='button1 btn btn-light rounded-pill my-2'>Sign up with Google</button>
-        <button className='button1 btn btn-light rounded-pill '>Sign up with Apple</button><br />
-        <div className="box"></div>
+        <p className='h1 ' >Happening now</p> <br /> <br />
+        <h1 className='h2'>Join today.</h1> <br />
+        <button className='button1 btn btn-light rounded-pill my-2 d-block'>
+          <i class="fa-brands fa-google" style={{marginRight : "15px" , fontSize : "20px"}}></i>
+          <p className='p1'>Sign up with Google</p></button>
+        <button className='button1 btn btn-light rounded-pill '>
+          <i class="fa-brands fa-apple" style={{marginRight : "20px" , fontSize : "25px"}}></i>
+          <p className='p1'>Sign up with Apple</p></button><br />
+        <div className="box"> </div>
         <p className='or'>or</p>
         <div className="box"></div>
-        <button className='button2 btn btn-primary rounded-pill my-3'>Create account</button>
+        <button className='button2 btn btn-primary rounded-pill '><p className='p1'>Create account</p></button>
         <p className='small'>By signing up, you agree to the <a href="">Terms of Service</a> and</p>
         <p className='small'><a href="">Privacy Policy</a>, including <a href="">Cookie Use.</a></p><br />
         <h5>Already have an account?</h5>
-        <button className='button3 text-primary rounded-pill'>Sign In</button>
+        <button className='button3 text-primary rounded-pill'><p className="p1">Sign In</p></button>
       </div>
     </div>
   )
